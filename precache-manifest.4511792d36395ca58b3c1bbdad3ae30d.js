0b47ef348ee597db72ac049824bd75d5e4e4e401
David Ahlstom <david0673@gmail.com>
2019-09-16 20:20:40 -0700

    Updates

---
+++ precache-manifest.4511792d36395ca58b3c1bbdad3ae30d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33c27aeff389238150641f2e560bb014",
+    "url": "/reactlab2/index.html"
+  },
+  {
+    "revision": "94db34948779ca87c6bb",
+    "url": "/reactlab2/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "12342e49a2e12a936a83",
+    "url": "/reactlab2/static/css/main.ab7171eb.chunk.css"
+  },
+  {
+    "revision": "94db34948779ca87c6bb",
+    "url": "/reactlab2/static/js/2.56f9d4af.chunk.js"
+  },
+  {
+    "revision": "12342e49a2e12a936a83",
+    "url": "/reactlab2/static/js/main.168846d4.chunk.js"
+  },
+  {
+    "revision": "0356698b026f8065afc5",
+    "url": "/reactlab2/static/js/runtime~main.02ebd656.js"
+  },
+  {
+    "revision": "b07728ccd0cb5dec811993e879c57567",
+    "url": "/reactlab2/static/media/building.b07728cc.jpg"
+  },
+  {
+    "revision": "69fb3cf87ba94920cbd2568b0a5825b5",
+    "url": "/reactlab2/static/media/myface.69fb3cf8.jpg"
+  },
+  {
+    "revision": "b285c4f8cca6555a78df6edbcdc71ba4",
+    "url": "/reactlab2/static/media/tech.b285c4f8.jpg"
+  }
+]);
